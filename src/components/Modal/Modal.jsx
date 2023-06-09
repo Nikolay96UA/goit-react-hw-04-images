19558625a3e12db3034d9a4e8286506950244e08
Nikolay Maloshenko <115625550+Nikolay96UA@users.noreply.github.com>
2023-06-10 00:24:59 +0300

    add start file

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import css from '../styles.module.css';
+
+
+export default class Modal extends React.Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleOverlayClick = event => {
+    if (event.target === event.currentTarget) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    const { image } = this.props;
+
+    return (
+      <div className={css.Overlay} onClick={this.handleOverlayClick}>
+        <div className={css.Modal}>
+          <img src={image} alt="" />
+        </div>
+      </div>
+    );
+  }
+}
